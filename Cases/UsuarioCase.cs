9652689820a3c3d76fc5f5e995581cbf33405f90
LeandroMPio <leandropio@gmail.com>
2023-11-13 12:46:35 -0300

    Add Filial Cases

--- Cases/UsuarioCase.cs
+++ Cases/UsuarioCase.cs
@@ -30,31 +30,39 @@ namespace CriaFolhetoOferta.Cases
         }
         public object EncontrarUmElemento(int identificacao)
         {
-           var usuarioEncontrado = Usuarios.FirstOrDefault(x => x.Id.Equals(identificacao));
-           return usuarioEncontrado;
+						var usuarioEncontrado = Usuarios.FirstOrDefault(x => x.Id.Equals(identificacao));
+						return usuarioEncontrado;
         }
         public void ListarUmElemento(int identificacao)
         {
-            Usuario usuario = (Usuario)EncontrarUmElemento(identificacao);
-            if(usuario == null)
+            Usuario Usuario = (Usuario)EncontrarUmElemento(identificacao);
+            if(Usuario == null)
             {
                 Console.WriteLine("Usuário não encontrado");
             }
             else
             {
                 Console.WriteLine($"Listando o usuário informado:");
-                Console.WriteLine($"Nome:\t{usuario.Nome}");
-                Console.WriteLine($"Login:\t{usuario.Login}");
-                Console.WriteLine($"Setor:\t{usuario.Setor}");
-                Console.WriteLine($"Filial:\t{usuario.Filial.Nome}");
+                Console.WriteLine($"Nome:\t{Usuario.Nome}");
+                Console.WriteLine($"Login:\t{Usuario.Login}");
+                Console.WriteLine($"Setor:\t{Usuario.Setor}");
+                Console.WriteLine($"Filial:\t{Usuario.Filial.Nome}");
+								Console.WriteLine("-------------------------------");                
             }
         }
 
         public void Deletar(int identificacao)
         {
-            Usuario usuario = (Usuario)EncontrarUmElemento(identificacao);
-            Usuarios.Remove(usuario);
-            Console.WriteLine($"Usuário {usuario.Nome} deletado");
+            Usuario Usuario = (Usuario)EncontrarUmElemento(identificacao);
+						if(Usuario == null)
+            {
+                Console.WriteLine("Usuário não encontrado");
+            }
+						else
+						{
+								Usuarios.Remove(Usuario);
+								Console.WriteLine($"Usuário {Usuario.Nome} deletado");
+						}
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -9,13 +9,28 @@ namespace CriaFolhetoOferta
             Filial CotiaCentro = new("Cotia centro", 59, "Av. Professor José Barreto 1217", "(11) 4666-9000");
             Usuario Leandro = new("Leandro", "leandr999999", "senha", "CPD", 1, CotiaCentro);
             UsuarioCase Usuarios = new();
+						FilialCase Filiais = new();
 
+
+						//Usuario
             Usuarios.Cadastrar(Leandro);
             Usuarios.ListarTodosElementos();
             Usuarios.ListarUmElemento(Leandro.Id);
             Usuarios.Deletar(Leandro.Id);
             Usuarios.ListarTodosElementos();
             Usuarios.ListarUmElemento(Leandro.Id);
+						Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+						//Filial
+						Filiais.Cadastrar(CotiaCentro);
+						Filiais.ListarTodosElementos();
+						Filiais.ListarUmElemento(CotiaCentro.Id);
+						Filiais.Deletar(CotiaCentro.Id);
+						Filiais.ListarTodosElementos();
+						Filiais.ListarUmElemento(CotiaCentro.Id);
+						Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+						Console.ReadLine();
+
         }
     }
 }
